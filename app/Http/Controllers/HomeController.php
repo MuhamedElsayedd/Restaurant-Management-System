488e11e537a34b57102689c3dd980b4748750fc3
Muhamed Elsayed <mohamedelsayed100100100@gmail.com>
2025-10-07 23:07:48 +0300

    Initial commit - upload Laravel project

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        if (Auth::id()) {
+            $usertype = Auth::user()->usertype;
+
+            if ($usertype == 'user') {
+                return view('dashboard');
+            } else {
+                return view('admin.index');
+            }
+        }
+    }
+}
